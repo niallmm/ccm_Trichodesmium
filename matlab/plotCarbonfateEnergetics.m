0ff72a70de46fb31ed77e66542eae912078e5d09
Avi Flamholz <flamholz@gmail.com>
2014-12-20 01:01:07 -0500

    plot the energetic cost breakdown as a function of pH.

--- matlab/plotCarbonfateEnergetics.m
+++ matlab/plotCarbonfateEnergetics.m
@@ -1,7 +1,54 @@
-% calculate pH from the kmH's selected
+% Cost of transport in the 3 cases (per fixation)
 cost_h = Hin./CratewO;
 cost_cy_h = Hin_cy./CratewO_cy;
 cost_just_rbc_h = Hin_just_rbc./CratewO_just_rbc;
+
+% Cost of fixation (per fixation)
+% 3 ATP, 2 NADPH per CO2 fixed in Calvin Cycle.
+% 4 H+ per ATP, 10 H+ per NADPH based on assumptions of
+% ATP synthase stoichiometry and ETC H+ pumping.
+cost_fixation = 4*3.0 + 10*2.0;
+
+% Cost of 2PG recovery through the C2 pathway is:
+% -- 1/2 fixed carbon (one decarboxylation for every two 2PG).
+% -- 1 ATP (one dephosphorylation for every 2PG). 
+% -- 1 NADH (one oxidation for every 2PG).
+% -- 1/2 fixed nitrogen (one net deamination for every two 2PG).
+% 4 H+ per ATP, 10 H+ per NADPH based on assumptions of
+% ATP synthase stoichiometry and ETC H+ pumping.
+% NOTE: Have not included cost of losing the amine. May be significant. 
+cost_per_2pg = (10 + 4 + 0.5*cost_fixation);
+
+% Fraction of RuBisCO reactions that are oxygenations.
+% Specificity ratio as defined by Savir et al. 2010. 
+% S = (RC/RO)([O2]/[CO2]) => RO/RC = [O2]/(S [CO2])
+frac_oxygenation_ccm = ccm_params.O ./ (Ccsome .* ccm_params.S_sat);
+frac_oxygenation_cy = ccm_params_cell.O ./ (Ccyto_cy .* ccm_params_cell.S_sat);
+frac_oxygenation_just_rbc = ccm_params_just_rbc.O ./ (Ccyto_just_rbc .* ccm_params_just_rbc.S_sat);
+
+% Cost of 2PG recovery on a per-fixation basis in each case.
+cost_2pg_ccm_h = cost_per_2pg * frac_oxygenation_ccm;
+cost_2pg_cy_h = cost_per_2pg * frac_oxygenation_cy;
+cost_2pg_just_rbc_h = cost_per_2pg * frac_oxygenation_just_rbc;
+
+% Total cost of the system in H+/fixation for each case.
+total_cost_ccm_h = cost_2pg_ccm_h + cost_h + cost_fixation;
+total_cost_cy_h = cost_2pg_cy_h + cost_cy_h + cost_fixation;
+total_cost_just_rbc_h = cost_2pg_just_rbc_h + cost_just_rbc_h + cost_fixation;
+
+% Cost breakdown of the ccm model as a function of pH.
+figure(5)
+area(pH, total_cost_ccm_h, 'FaceColor', 'm', 'BaseValue', 1e-1);
+hold on;
+area(pH, cost_fixation + cost_2pg_ccm_h, 'FaceColor', 'g', 'BaseValue', 1e-1);
+area(pH, cost_2pg_ccm_h, 'FaceColor', 'k', 'BaseValue', 1e-1);
+plot(pH, total_cost_ccm_h, '-k', 'LineWidth', 3);
+set(gca, 'Yscale', 'log');
+axis([pH(1) pH(end) 1e-1 2e7]);
+ylabel('Energetic Cost H^+ / (CO_2 fixed)');
+xlabel('Cytoplasmic pH');
+title('Energetic Cost of the Cyanobacterial CCM');
+legend('Transport', 'Fixation', 'Photorespiration', 'Total Cost');
 
 figure(2)
 loglog(kmH, cost_h, 'Color', 'k', 'LineWidth', 3);
@@ -15,9 +62,7 @@ text(1.7e-7, 5e4, 'HCO_3^- permeability', 'Color', 'm')
 plot([3e-3 3e-3], [1e-1 2e7], 'Color', 'c', 'LineWidth', 20)
 text(7e-5, 5e4, 'H_2CO_3 permeability', 'Color', 'c')
 
-% 3 ATP, 2 NADPH per CO2 fixed in Calvin Cycle.
-% 4 H+ per ATP, 10 H+ per NADPH. 
-cost_fixation = 4*3.0 + 10*2.0;
+
 plot([kmH(1) kmH(end)], [cost_fixation cost_fixation], '--b');
 %text(kmH(1)/1100.0, 50, 'Calvin Cycle Fixation', 'Color', 'b');
 
