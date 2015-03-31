8a7f6c87ab2a96ab2036bfe252152cd28b775ff3
niallmm <niallmm@gmail.com>
2015-03-31 17:56:26 -0400

    files to plot
    Gradients across the cell
    and the critical transitions for RuBisCO and carbonic anhydrase
    saturating

--- matlab/GradientsAcrossCell.m
+++ matlab/GradientsAcrossCell.m
@@ -1,6 +1,7 @@
 % Check Gradients across cell
     p = CCMParams_Csome;
-    p.jc = 0.0007;
+    p.jc = 3.5e-4;
+    p.k = 1e-4;
     p.pH = 8;
     exec = FullCCMModelExecutor(p);
     res = exec.RunAnalytical();
@@ -18,8 +19,16 @@ Hcyto = ((p.jc+p.kmH)*p.Hout + p.alpha*res.c_cyto_uM - p.kmH*res.h_csome_uM)*...
     
 
 figure
-plot(rb, Ccyto*1e-3, 'r')
+semilogy(rb, Ccyto*1e-3, 'r')
 hold on
 plot(rb, Hcyto*1e-3, 'b')
 line([0 p.Rc],[Ccsome Ccsome], 'Color','r') 
 line([0 p.Rc], [Hcsome Hcsome], 'Color', 'b')
+xlabel('Cell radius (cm)')
+ylabel('Concentration (mM)')
+ line([Rc Rc], [1e-6 1e6], 'Color', [0.5 0.5 0.5], 'LineStyle', '-.', 'LineWidth', 3)
+xlim([0 5e-5])
+ylim([1e-5 100])
+set(gca,'XTick',[0 1e-5 2e-5 3e-5 4e-5 5e-5])
+set(gca,'YTick',[1e-5  1e-3  1e-1  10])
+box off
--- matlab/PlottingFigures/critical_jc_vs_kc.m
+++ matlab/PlottingFigures/critical_jc_vs_kc.m
@@ -6,55 +6,51 @@ exec = FullCCMModelExecutor(p);
 res = exec.RunAnalytical();
        
         
-k = logspace(-9,2, 1e4);
+kvec = logspace(-9,2, 1e4);
+
+for i = 1:length(kvec)
+    p.k = kvec(i);
+exec = FullCCMModelExecutor(p);
+res = exec.RunAnalytical();
 
 
-G = D./(k*Rc^2) + 1/Rc - 1/Rb;
-
-
-M = (kmC + alpha)*(1+Vca*Kba/(Vba*Kca))*kmH*G + ...
-    kmC*(1+kmH*Vca*Kba/(kmC*Vba*Kca))*D/Rb^2;
-P = ((alpha + kmC)*G + D/Rb^2).*(kmH*G + D/Rb^2);
-
-
-CCAsat0 = Vba*(Rc^3)*(G+D/((alpha+kmC)*Rb^2))/(3*D) + ...
-            Vba*(Rc^2)/(6*D) + kmC*Cout/(alpha+kmC);
 
 % Critical jc where CA 'becomes saturated'        
 
-Ccrit = CCAsat0;
-critjc= (M.*Ccrit + Vmax*Ccrit.*P*Rc^3./(3*D*(Ccrit+Km))- ...
-            kmC*Cout*((kmH+alpha)*G +D/Rb^2))./...
-           (Hout*((kmC+alpha)*G + D/Rb^2)) - kmH;
+Ccrit = res.CCAsat0;
+critjc(i)= (res.M.*Ccrit + p.Vmax*Ccrit.*res.P*p.Rc^3./(3*p.D*(Ccrit+p.Km))- ...
+            p.kmC*p.Cout*((p.kmH+p.alpha)*p.G +p.D/p.Rb^2))./...
+           (p.Hout*((p.kmC+p.alpha)*p.G + p.D/p.Rb^2)) - p.kmH;
         
 % critical jc were Rubisco is saturated
-Ccrit = Km;
-critjcRub= (M*Ccrit + Vmax*Ccrit*P*Rc^3/(3*D*(Ccrit+Km))- ...
-            kmC*Cout*((kmH+alpha)*G +D/Rb^2))./...
-           (Hout*((kmC+alpha)*G + D/Rb^2)) - kmH;
+Ccrit = p.Km;
+critjcRub(i)= (res.M.*Ccrit + p.Vmax*Ccrit.*res.P*p.Rc^3./(3*p.D*(Ccrit+p.Km))- ...
+            p.kmC*p.Cout*((p.kmH+p.alpha)*p.G +p.D/p.Rb^2))./...
+           (p.Hout*((p.kmC+p.alpha)*p.G + p.D/p.Rb^2)) - p.kmH;
         
 % calculate jc where carboxylation is 99% 
 % when RuBisCO is saturated: S = vc/vo ~ 13;
 % not saturated: S = (vc/Kc)/(vo/Ko)~ 50 %SavirMilo paper
-% carboxylation/oxygenation = S*[CO_2]/[O_2] -> 
+% carboxylation/oxyp.Genation = S*[CO_2]/[O_2] -> 
 % CO_2 needed to make carboxylation 99% -> 99*[O_2]/S
 O2 = 260; % uM (Savir Milo paper)
 S = 13; % when RuBisCO is saturated
 
 Ccrit = 99*O2/S;
-critjcRub99= (M*Ccrit + Vmax*Ccrit*P*Rc^3/(3*D*(Ccrit+Km))- ...
-            kmC*Cout*((kmH+alpha)*G +D/Rb^2))./...
-           (Hout*((kmC+alpha)*G + D/Rb^2)) - kmH;
+critjcRub99(i)= (res.M.*Ccrit + p.Vmax*Ccrit.*res.P*p.Rc^3./(3*p.D*(Ccrit+p.Km))- ...
+            p.kmC*p.Cout*((p.kmH+p.alpha)*p.G +p.D/p.Rb^2))./...
+           (p.Hout*((p.kmC+p.alpha)*p.G + p.D/p.Rb^2)) - p.kmH;
         
 Ccrit = 999*O2/13;
-critjcRub999= (M*Ccrit + Vmax*Ccrit*P*Rc^3/(3*D*(Ccrit+Km))- ...
-            kmC*Cout*((kmH+alpha)*G +D/Rb^2))./...
-           (Hout*((kmC+alpha)*G + D/Rb^2)) - kmH;
-figure(3)        
-loglog(critjc, k, '-k')
+critjcRub999(i)= (res.M.*Ccrit + p.Vmax*Ccrit.*res.P*p.Rc^3./(3*p.D*(Ccrit+p.Km))- ...
+            p.kmC*p.Cout*((p.kmH+p.alpha)*p.G +p.D/p.Rb^2))./...
+           (p.Hout*((p.kmC+p.alpha)*p.G + p.D/p.Rb^2)) - p.kmH;
+end
+figure(5)        
+loglog(critjc, kvec, '-k')
 hold on
-plot(critjcRub, k, '-r')
-plot(critjcRub99, k, '-c')
+plot(critjcRub, kvec, '-r')
+plot(critjcRub99, kvec, '-c')
 % plot(critjcRub999, k, '-g')
 xlabel('Active HCO_3^- transport, j_c, (cm/s)')
 ylabel('Carboxysome permeability')
